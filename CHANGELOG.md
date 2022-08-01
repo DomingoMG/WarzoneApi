bd33ffc943f8ba517eae04e97a3f2c8e8c7df0b2
DomingoMG <domingomg97@gmail.com>
2022-08-01 22:50:09 +0100

    Readme and Example updated

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
-## 1.0.0
+## 1.0.1
+- added example
+- The readme has been improved
 
+## 1.0.0
 - Initial version.
--- example/warzone_api_example.dart
+++ example/warzone_api_example.dart
@@ -1,3 +1,15 @@
-void main() {
- 
+import 'package:warzone_api/src/enums/enums.dart';
+import 'package:warzone_api/src/models/warzone_data.dart';
+import 'package:warzone_api/src/warzone_api_base.dart';
+
+void main() async {
+  
+  /// [WarzoneApi] Instance
+  WarzoneApi warzoneApi = WarzoneApi();
+  
+  /// [WarzoneData] All user data found
+  WarzoneData? warzoneData = await warzoneApi.search(username: 'RaiiLKilleR#8661004', platform: Platform.atvi);
+
+  // Show data
+  print(warzoneData.toMap());
 }
